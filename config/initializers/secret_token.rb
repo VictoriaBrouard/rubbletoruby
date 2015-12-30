26c5f258e6fda057e81b4d1bf909323d820bdde9
Victoria Brouard <victoria_brouard@hotmail.co.uk>
2015-12-30 19:46:11 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rubbletorubyblog::Application.config.secret_key_base = '096db0714d76187d196d34f03a582b436c963e4c773fe767d1fbd9474aa476b93ce431f105e1a1de4af3eb0e36df5fe3fc00a3daa797d39be72df942a339bae9'
